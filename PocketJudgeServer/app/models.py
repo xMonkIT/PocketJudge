3ecd201879f8fa7984dbf70126f051a2ce0dec9b
Владимир Омелин < >
2016-11-21 13:49:15 +0300

    add models

--- PocketJudgeServer/app/models.py
+++ PocketJudgeServer/app/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Contestant(models.Model):
+    user = models.OneToOneField(User)
+
+
+class Judge(models.Model):
+    user = models.OneToOneField(User)
+
+
+class Contest(models.Model):
+    name = models.TextField(max_length=255)
+    parent_contest = models.ForeignKey('self', null=True, blank=True)
+    judges = models.ManyToManyField(Judge)
+
+
+class Competence(models.Model):
+    name = models.TextField(max_length=255)
+    max_mark = models.IntegerField(default=10)
+    contest = models.ForeignKey(Contest)
+
+
+class Project(models.Model):
+    name = models.TextField(max_length=255)
+    description = models.TextField(max_length=1024, null=True, blank=True)
+    contestants = models.ManyToManyField(Contestant)
+
+
+class Mark(models.Model):
+    mark = models.IntegerField()
+    competence = models.ForeignKey(Competence)
+    project = models.ForeignKey(Project)
+    judge = models.ForeignKey(Judge)
+
+    class Meta:
+        unique_together = ("competence", "project", "judge")
