0527ddd067606b1bde61493b4f1315a2623e765f
xMonkIT <omelin.vladimir@gmail.com>
2017-04-11 13:54:43 +0300

    remove redundant form

--- PocketJudgeServer/app/forms.py
+++ PocketJudgeServer/app/forms.py
@@ -17,15 +17,3 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder': 'Пароль'}))
-
-
-class BootstrapInlineAuthenticationForm(AuthenticationForm):
-    """Authentication form which uses bootstrap CSS."""
-    username = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'Имя пользователя'}))
-    password = forms.CharField(label=_("Password"),
-                               widget=forms.PasswordInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'Пароль'}))
